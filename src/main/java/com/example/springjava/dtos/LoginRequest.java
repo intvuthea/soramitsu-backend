98b8c287cdb0d9a58c071c47056de0333a55ea77
Vuthea <intvuthea007@gmail.com>
2022-07-15 01:47:03 +0700

    update

--- src/main/java/com/example/springjava/dtos/LoginRequest.java
+++ src/main/java/com/example/springjava/dtos/LoginRequest.java
@@ -3,9 +3,14 @@ package com.example.springjava.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class LoginRequest {
+    @NotNull(message = "username is required")
     private String username;
+
+    @NotNull(message = "password is required")
     private String password;
 }
